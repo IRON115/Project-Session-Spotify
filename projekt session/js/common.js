eb563d7d85ebf7a5b9a70ca07c211189327049e7
Kakao <98341086+IRON115@users.noreply.github.com>
2023-06-29 21:18:20 +0300

    Add files via upload

---
+++ projekt session/js/common.js
@@ -0,0 +1,28 @@
+/*$(function() {
+
+	new WOW().init();
+
+	$(".hamburger").click(function(){
+		$(this).toggleClass("is-active");
+
+		if($(this).hasClass('is-active')){
+			$('.mnu_top').slideDown(300);
+		}else{
+			$('.mnu_top').slideUp(300);
+		}
+	});
+
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".items");
+
+hamburger.addEventListener("click", () =>{
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".links").forEach(n => n.addEventListener("click", () =>{
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");	
+}))*/
